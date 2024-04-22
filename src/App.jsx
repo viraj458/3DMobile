1f7fd591dbf3e9f24484929e195323b6b2c4bc95
viraj458 <virajmalaka94@gmail.com>
2024-04-22 12:50:09 +0530

    feat: Add Chip section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Chip from './components/Chip';
 import Features from './components/Features';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <Chip />
     </main>
   );
 };
